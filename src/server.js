f4547e84629aa53c04e1aae0f2f93edeca06c70e
mikekang47 <gyeong0308@gmail.com>
2022-03-21 19:54:01 +0900

    Feat: set get mapping

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", __dirname+ "/views");
 
-console.log("hello world!");
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
