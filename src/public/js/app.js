aa0e3bb05e0097af2387ab3d64b57cedcc5a4d2f
mikekang47 <gyeong0308@gmail.com>
2022-03-22 04:05:47 +0900

    Feat: add connection message in app.js

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,16 @@
-alert("hi");
+
+const url = window.location.host;
+
+const socket = new WebSocket(`ws://${url}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅");
+})
+
+socket.addEventListener("message", (message) => {
+    console.log("Just got this ", message.data , " form the Server");
+})
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+})
