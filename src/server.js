e56df365c4c7a36697045a8792c326a5a3199c19
sihoo kang <70617238+mikekang47@users.noreply.github.com>
2022-03-23 02:04:46 +0900

    Revert "Day1"

--- src/server.js
+++ src/server.js
@@ -1,28 +1,9 @@
 import express from "express";
-import http from "http";
-import { connect } from "net";
-import WebSocket from "ws";
+
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", __dirname+ "/views");
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+console.log("hello world!");
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", (socket)=> {
-    console.log("Connected to Browser✅");
-    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-    socket.on("message", message => {
-        console.log(message.toString("utf-8"));
-    })
-    socket.send("welcome");
-});
-
-server.listen(3000, handleListen);
+app.listen(3000);
